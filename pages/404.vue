a5f18e5db15b20f8198a988aa0d581f2e6d98035
Hossein Heidari <squanderness@gmail.com>
2023-11-02 06:45:46 +0330

    404

---
+++ pages/404.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="text-paper_lt w-full h-[100vh] flex flex-col items-center justify-center"
+  >
+    <div class="text-[20vh] font-[BigShoulders] font-black">404</div>
+    <div class="text-lg font-normal">NOT FOUND</div>
+    <NuxtLink class="text-lg font-normal mt-8 underline" to="/"
+      >GO BACK HOME</NuxtLink
+    >
+  </div>
+</template>
+
+<script lang="ts" setup>
+onBeforeMount(() => {
+  useBackground("dark")
+})
+
+onUnmounted(() => {
+  useBackground("light")
+})
+
+definePageMeta({
+  layout: false,
+})
+</script>
+
+<style></style>
