ee49a89df2671ea1d5c60aa6a0a81c1ff88fbf4c
Hossein Heidari <squanderness@gmail.com>
2023-11-20 15:59:32 +0330

    rm vsharp

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import vsharp from "vite-plugin-vsharp"
+// import vsharp from "vite-plugin-vsharp"
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/global.css"],
   build: { transpile: ["gsap"] },
   modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
-  vite: { plugins: [vsharp()] },
+  // vite: { plugins: [vsharp()] },
 })
