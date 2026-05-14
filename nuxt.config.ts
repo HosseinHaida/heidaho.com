8cf18faa59fe7b8d96d74f1a03f841aae30d98d0
Hossein Heidari <squanderness@gmail.com>
2026-05-14 22:45:24 +0200

    chore(deps): update packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import vsharp from "vite-plugin-vsharp"
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer"
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/global.css"],
   build: { transpile: ["gsap"] },
-  modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
-  vite: { plugins: [vsharp()] },
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon"],
+  vite: { plugins: [ViteImageOptimizer()] },
 })
